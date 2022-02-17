eb9b40b873e3cd534435352068159b9ebeb38649
bengetacademy <ben@getacademy.no>
2022-02-17 12:47:38 +0100

    updated beerdetails to use components

--- src/views/Beer/BeerDetails.vue
+++ src/views/Beer/BeerDetails.vue
@@ -5,40 +5,41 @@
     <h2>ABV: {{ beer.abv }}</h2>
     <p>This is beer id: {{ beer.id }}</p>
     <p>{{ beer.description }}</p>
-    <h1>Hops</h1>
-    <ul>
-      <li v-for="(hop, index) in beer.ingredients.hops" :key="index">
-        <p>{{ hop.name }}</p>
-      </li>
-    </ul>
-    <h1>Malt</h1>
-    <ul>
-      <li v-for="(malt, index) in beer.ingredients.malt" :key="index">
-        <p>{{ malt.name }}</p>
-      </li>
-    </ul>
-    <h1>Methods</h1>
-    <ul>
-      <li v-for="(method, key) in beer.method" :key="key">
-        <!-- Beer Method Component here. -->
-        <p v-if="method">Beer Method Component here. {{ key }} {{ method }}</p>
-      </li>
-    </ul>
+    <div>
+      <h1>Hops</h1>
+      <BeerHopList :hops="beer.ingredients.hops" />
+    </div>
+    <div>
+      <h1>Malt</h1>
+      <BeerMaltList :malt="beer.ingredients.malt" />
+    </div>
+    <div>
+      <h1>Methods</h1>
+      <BeerMethodList :methods="beer.method" />
+    </div>
   </div>
 </template>
 
 <script>
-const axios = require("axios");
+const axios = require('axios');
+import BeerHopList from '@/components/BeerHopList.vue';
+import BeerMaltList from '@/components/BeerMaltList.vue';
+import BeerMethodList from '@/components/BeerMethodList.vue';
 
 export default {
-  props: ["id"],
+  props: ['id'],
+  components: {
+    BeerHopList,
+    BeerMaltList,
+    BeerMethodList,
+  },
   data() {
     return {
       beer: null,
     };
   },
   async mounted() {
-    let beer = await axios.get("https://api.punkapi.com/v2/beers/" + this.id);
+    let beer = await axios.get('https://api.punkapi.com/v2/beers/' + this.id);
     this.beer = beer.data[0];
     console.log(beer.data);
   },
